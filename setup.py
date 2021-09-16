4a898988e02587f82da1b46e52c0e715d0005f8d
tjsego <tjsego@gmail.com>
2021-09-16 07:03:20 -0400

    Implemented conda package

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+version_path = os.path.join(rootdir, 'VERSION.txt')
+player_version = open(version_path).readline().strip()
+
+setup(name='cc3d-player5',
+      author='T.J. Sego, Maciek Swat',
+      classifiers=[
+            'Intended Audience :: Science/Research'
+      ],
+      description='CC3D Player: a real-time, interactive CompuCell3D simulation execution and visualization graphical user interface',
+      url='https://compucell3d.org',
+      version=player_version,
+      packages=find_packages(
+            include=['cc3d.player5', 'cc3d.player5.*']
+      ),
+      include_package_data=True,
+      package_data={
+            '': [
+                  '*.information', '*.qrc', '*.sql', '*.xml'
+            ],
+            'cc3d.player5': [
+                  'Configuration/*',
+                  'Configuration_settings/*',
+                  'Configuration_settings/osx/*',
+                  'icons/*',
+                  'Launchers/*',
+                  'Plugins/ViewManagerPlugins/*'
+            ]
+      },
+      entry_points={
+            'gui_scripts': ['cc3d-player5ParamScan = cc3d.player5.param_scan.parameter_scan_run:main',
+                            'cc3d-player5 = cc3d.player5.__main__:main']
+      }
+      )
