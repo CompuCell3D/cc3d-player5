8f98181baaa9d7a2e4c5d71a7b64cf17ed4e01d3
compucell3d <maciekswat@gmail.com>
2023-09-02 10:43:37 -0600

    minor cleanup

--- cc3d/player5/UI/ErrorConsole.py
+++ cc3d/player5/UI/ErrorConsole.py
@@ -13,8 +13,6 @@ from PyQt5 import Qsci
 from PyQt5 import QtGui
 
 from cc3d.player5.Messaging import stdMsg, dbgMsg, pd, errMsg, setDebugging
-# from QsciScintillaCustom import QsciScintillaCustom
-# from PyQt4.Qsci.QsciScintilla import *
 # NOTICE: complete scintilla documentation can be found here:
 # http://www.scintilla.org/ScintillaDoc.htm
 from . import CC3DSender
