7c05565c173500fa6ee7f4ae8809413581547612
Pete Fyffe <fyffep>
2023-11-20 17:00:42 -0500

    Added button to open movie dir

--- cc3d/player5/Plugins/ViewManagerPlugins/MovieMediator.py
+++ cc3d/player5/Plugins/ViewManagerPlugins/MovieMediator.py
@@ -1,5 +1,8 @@
 import os
+import sys
+import subprocess
 import cc3d.player5.Configuration as Configuration
+from PyQt5.QtWidgets import QMessageBox
 from cc3d.core.GraphicsUtils.MovieCreator import makeMovie
 
 def makeMovieWithSettings():
@@ -18,4 +21,31 @@ def makeMovieWithSettings():
     quality = Configuration.getSetting("Quality")
     writeText = Configuration.getSetting("WriteMovieMCS")
 
-    return makeMovie(simulationPath, frameRate, quality, writeText)
+    numMoviesMade, moviePath = makeMovie(simulationPath, frameRate, quality, writeText)
+
+    Configuration.setSetting("RecentMoviePath", moviePath)
+
+    return numMoviesMade, moviePath
+
+
+def showMovieInFileExplorer(self):
+    if Configuration.check_if_setting_exists("RecentMoviePath"):
+        dirToOpen = Configuration.getSetting("RecentMoviePath")
+    elif Configuration.check_if_setting_exists("OutputLocation"):
+        dirToOpen = Configuration.getSetting("OutputLocation")
+
+    if not os.path.exists(dirToOpen):
+        dirToOpen = "/"
+
+    if sys.platform.startswith('win'):
+        subprocess.run(['explorer', '/select,', dirToOpen], shell=True)
+    elif sys.platform.startswith('darwin'):
+        subprocess.run(['open', '-R', dirToOpen])
+    elif sys.platform.startswith('linux'):
+        QMessageBox.warning(None, "WARN",
+                            "Linux is not supported.",
+                            QMessageBox.Ok)
+    else:
+        QMessageBox.warning(None, "WARN",
+                            "Your operating system is not supported.",
+                            QMessageBox.Ok)
