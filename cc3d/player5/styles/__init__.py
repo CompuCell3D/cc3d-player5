8ffcbda4ce9eab761c2b930ed72f7d75f707c50b
Pete Fyffe <fyffep>
2023-12-07 19:45:52 -0500

    Added first stylesheet:

--- cc3d/player5/styles/__init__.py
+++ cc3d/player5/styles/__init__.py
@@ -1,1 +1,57 @@
-tab_bar_style = "QTabBar::tab:selected {color: #00ff00;background-color: rgb(0,0,255);}"
+tab_bar_style = """
+* {
+    color: white;
+    background-color: rgb(37, 37, 37);
+    font-family: Verdana;
+}
+
+QDialog { 
+    background-color: rgb(37, 37, 37);
+}
+
+QPushButton {
+    text-align: left;
+    border-left: none;
+    border-right: none;
+    border-top: none;
+    border-bottom: 4px solid red;
+    padding:  4px 12px 4px 12px;
+    background-color: rgb(74, 74, 74);
+}
+
+QPushButton:active {
+    border-bottom: none;
+}
+
+QSpinBox, QLineEdit {
+    border-radius: 3px;
+    border: 1px solid #AAAAAA;
+    background-color: rgba(0,0,0,0);
+}
+
+QSpinBox:focus, QLineEdit:focus {
+    background-color: rgb(74, 74, 74);
+}
+
+QTabWidget::pane {
+    background-color: rgb(64, 64, 64);
+}
+
+QTabBar::tab {
+    background-color: rgb(54, 54, 54);
+    border: 1px solid rgb(74, 74, 74);
+    padding: 4px;
+    border-radius: 2px;
+}
+
+QTabBar::tab:selected {
+    background-color: rgb(116, 205, 207);
+    margin-bottom: -1px;
+    margin-top: 1px;
+}
+
+#line_4, #line_5, #line_3 {
+    background-color: rgb(104, 104, 104);
+}
+"""
+
