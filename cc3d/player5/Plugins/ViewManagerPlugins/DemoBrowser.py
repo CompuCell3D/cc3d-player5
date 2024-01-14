82593a37091ff42f77f1210beb5cbe6a4a7bc987
Pete Fyffe <pgf01@rocketmail.com>
2024-01-14 16:56:45 -0500

    Added sorting by search index scores to Demo Browser

--- cc3d/player5/Plugins/ViewManagerPlugins/DemoBrowser.py
+++ cc3d/player5/Plugins/ViewManagerPlugins/DemoBrowser.py
@@ -6,14 +6,16 @@ from cc3d.player5.Plugins.ViewManagerPlugins import ui_demo_browser
 import weakref
 import re
 from pathlib import Path
-import time
 from collections import Counter
 
 from cc3d.player5.styles.StyleManager import subscribeToStylesheet
 
-            
+
+def getDemoRootPath():
+    return Path("C:\\Users\\Pete\\Documents\\cc3d\\CompuCell3D\\CompuCell3D\\core\\Demos")
+
 def getDemoList():
-    rootPath = Path("C:\\Users\\Pete\\Documents\\cc3d\\CompuCell3D\\CompuCell3D\\core\\Demos") #FIXME/TODO/TEMP
+    rootPath = getDemoRootPath()
     globResults = rootPath.rglob('*.cc3d')
     globResults = [p.relative_to(rootPath) for p in globResults]
 
@@ -85,92 +87,27 @@ def tokenizeXml(line):
 searchIndex = {} #maps key words to lists of paths to demos
 
 def preIndexSearchResults():
-    allFreq = {}
+    MAX_MATCHES = 7 #how many demos each keyword can map to
 
-    rootPath = Path("C:\\Users\\Pete\\Documents\\cc3d\\CompuCell3D\\CompuCell3D\\core\\Demos")  
+    rootPath = getDemoRootPath()  
     demoList = list(getDemoList())
                 
     for demoPath in demoList:
         fullPath = rootPath.joinpath(demoPath)
         
-        # freqByFile = {}
-        # for word in tokenizeAllFiles(fullPath):
-        #     if word not in freqByFile:
-        #         freqByFile[word] = 1
-        #     else:
-        #         freqByFile[word] += 1
-            
-        # for (word, freq) in freqByFile:
-        #     if word not in allFreq:
-        #         allFreq[word] = 1
-        #     else:
-        #         allFreq[word] += 1
-        
-        #Find how frequently each word in `freqByFile` occurred across all files (allFreq).
-        # keywords = [(word, allFreq[word]) for word in freqByFile.keys()]
-        # keywords = sorted(keywords, key=lambda x: x[1])
-        # keywords = [row[0] for row in keywords]
-                
-        # keywords = list(freqByFile.keys())
-
         wordsSeen = set()
-
         for word in tokenizeAllFiles(fullPath):
             if word not in wordsSeen:
                 smallPath = str(demoPath) #conserve memory by converting object to str
-                if word in searchIndex and len(searchIndex[word]) < 5:
+                if word in searchIndex and len(searchIndex[word]) < MAX_MATCHES:
                     searchIndex[word].append(smallPath)
                 else:
                     searchIndex[word] = [smallPath]
                 wordsSeen.add(word)
             
 
-# for i in range(1):
-#     start_time = time.time()
-#     preIndexSearchResults()
-#     print("--- %s seconds ---" % (time.time() - start_time))
-
 preIndexSearchResults()
 print("Done indexing demos.")
-
-# for keyword in sorted(list(searchIndex.keys())):
-#     print(keyword)
-
-
-
-
-import sys
-from types import ModuleType, FunctionType
-from gc import get_referents
-
-# Custom objects know their class.
-# Function objects seem to know way too much, including modules.
-# Exclude modules as well.
-BLACKLIST = type, ModuleType, FunctionType
-
-
-def getsize(obj):
-    """sum size of object & members."""
-    if isinstance(obj, BLACKLIST):
-        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
-    seen_ids = set()
-    size = 0
-    objects = [obj]
-    while objects:
-        need_referents = []
-        for obj in objects:
-            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
-                seen_ids.add(id(obj))
-                size += sys.getsizeof(obj)
-                need_referents.append(obj)
-        objects = get_referents(*need_referents)
-    return size
-print("SIZEOF: ",getsize(searchIndex)) #139342
-
-
-
-
-
 
 
 
@@ -183,7 +120,6 @@ class DemoBrowser(QDialog, ui_demo_browser.Ui_demoDialog):
         self.model = None
         self.view = None
 
-        print("init")
         if sys.platform.startswith('win'):
             #Display dialog without context help - only close button exists
             self.setWindowFlags(Qt.Drawer)
@@ -195,60 +131,49 @@ class DemoBrowser(QDialog, ui_demo_browser.Ui_demoDialog):
         
         self.searchLineEdit.textChanged.connect(self.filterByKeyWord)
 
-        print("Registering")
-        self.filterByKeyWord() #TEMP
-        print("Registered")
+        self.filterByKeyWord()
 
     def filterByKeyWord(self):
         searchText = str(self.searchLineEdit.text())
-        if not searchText:
-            searchText = "cartesian" #TEMP
+        # if not searchText:
+        #     searchText = "chemotaxis" #TEMP
         searchText = searchText.strip().lower()
-        print("searchText",searchText)
-
-        rootPath = Path("C:\\Users\\Pete\\Documents\\cc3d\\CompuCell3D\\CompuCell3D\\core\\Demos")  
 
         demoList = list(getDemoList())
 
         if searchText:
             searchKeywords = [w for w in splitByUppercase(searchText)]
             searchKeywords.extend(searchText.split())
-            #list(dict.fromkeys(mylist))
-            print("searchKeywords ------>",searchKeywords)
 
-            goodPaths = []
+            keywordMatchPaths = []
             for word in searchKeywords:
                 if word in searchIndex:
-                    print("found via searchIndex:",searchIndex[word])
-                    goodPaths.extend(searchIndex[word])
-            print('goodPaths',goodPaths)
-            goodPaths = Counter(goodPaths)
-            scores = dict(goodPaths)
+                    keywordMatchPaths.extend(searchIndex[word])
+            
+            keywordMatchPaths = [Path(x) for x in keywordMatchPaths]
+            keywordMatchPaths = Counter(keywordMatchPaths)
+            scores = dict(keywordMatchPaths)
 
-            for demoName in demoList:
-                prettyName = formatDemoName(str(demoName.stem)).lower()
+            for demoPath in demoList:
+                prettyName = formatDemoName(str(demoPath.stem)).lower()
                 for word in searchKeywords:
-                    if word in str(demoName).lower() or prettyName.startswith(word):
-                        if demoName in scores:
-                            scores[demoName] += 1
+                    if word in str(demoPath).lower() or prettyName.startswith(word):
+                        if demoPath in scores:
+                            scores[demoPath] += 1
                         else:
-                            scores[demoName] = 1
-                
-            print("scores",scores)
+                            scores[demoPath] = 1
+            
+            self.bestDemoPaths = sorted(scores, key=scores.get, reverse=True)
         else:
             #Default to alphabetical
             demoNames = [x.stem for x in demoList]
+            self.bestDemoPaths = [p for _, p in sorted(zip(demoNames, demoList))]
             
-            #FIXME
-            relativePaths = [p for _, p in sorted(zip(demoNames, demoList))]
-            self.absPaths = [rootPath.joinpath(p) for _, p in sorted(zip(demoNames, demoList))]
 
         #Render the filtered results
         model = QStringListModel()
-        # model.setStringList([formatDemoName(item.stem) for item in relativePaths])
-        stringModel = [formatDemoName(Path(key).stem) for key, value in scores.items() if value > 0]
-        print(stringModel)
-        model.setStringList(stringModel)
+        bestDemoNames = [formatDemoName(p.stem) for p in self.bestDemoPaths]
+        model.setStringList(bestDemoNames)
 
         self.demoListView.setModel(model)
         self.model = model
@@ -256,8 +181,12 @@ class DemoBrowser(QDialog, ui_demo_browser.Ui_demoDialog):
 
     
     def selectDemo(self, selected):
-        treeIndex = selected.indexes()[0]
-        demoPath = self.absPaths[treeIndex.row()]
+        try:
+            treeIndex = selected.indexes()[0]
+            relPath = self.bestDemoPaths[treeIndex.row()]
+            absPath = getDemoRootPath().joinpath(relPath)
+        except KeyError:
+            print("Error: Could not select that demo")
 
-        print("Clicked on ",demoPath)
+        print("Clicked on ",absPath)
         
