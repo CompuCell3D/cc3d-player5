4a17610b04bdfaa846b4a8564c3c3dde32cbdecb
compucell3d <maciekswat@gmail.com>
2024-01-20 14:42:37 -0700

    Added ThemeName to be global-only setting. Made provisional fixes to avoid styles mixe up (light mixed with dark) . Added Default Theme. Also removed reusable BaseStyles.css

--- cc3d/player5/styles/StyleManager.py
+++ cc3d/player5/styles/StyleManager.py
@@ -8,6 +8,7 @@ import weakref
 from cc3d.player5 import Configuration
 
 BASE_STYLES_FNAME = "BaseStyles"
+DEFAULT_STYLE_FNAME = "DefaultTheme"
 
 styleManagerPath = Path(__file__).parents[0]
 themeDir = styleManagerPath.joinpath('themes')
@@ -44,59 +45,95 @@ Returns a combination of BaseStyles.css and the
 current theme's CSS file contents.
 :_themeName: the name of a .css file without its extension, such as "LightTheme"
 """
+# def _getStyleSheet(_themeName):
+#     global currentTheme
+#     if _themeName == currentTheme and currentStyleSheet:
+#         return currentStyleSheet
+#     else:
+#         themePath = Path(themeDir).joinpath(_themeName)
+#         themePath = themePath.with_suffix(".css")
+#         if Path.exists(themePath):
+#             currentTheme = _themeName
+#             with open(themePath, 'r') as themeFile:
+#                 customStyles = themeFile.read().replace('\n', '')
+#                 # Just a concatenation between stylesheets...
+#                 # it is slow but effective.
+#
+#                 if _themeName != DEFAULT_STYLE_FNAME:
+#                     return _getBaseStylesheet() + customStyles
+#                 else:
+#                     return  customStyles
+#         else:
+#             print(type(_themeName))
+#             print('Could not find theme: ' + _themeName + ' in the styles directory.')
+#             return ""
+
 def _getStyleSheet(_themeName):
-    global currentTheme
-    if _themeName == currentTheme and currentStyleSheet:
-        return currentStyleSheet
-    else:
-        themePath = Path(themeDir).joinpath(_themeName)
-        themePath = themePath.with_suffix(".css")
-        if Path.exists(themePath):
-            currentTheme = _themeName
-            with open(themePath, 'r') as themeFile:
-                customStyles = themeFile.read().replace('\n', '')
-                # Just a concatenation between stylesheets...
-                # it is slow but effective.
+    # global currentTheme
+    # if _themeName == currentTheme and currentStyleSheet:
+    #     return currentStyleSheet
+    # else:
+    themePath = Path(themeDir).joinpath(_themeName)
+    themePath = themePath.with_suffix(".css")
+    if Path.exists(themePath):
+        currentTheme = _themeName
+        with open(themePath, 'r') as themeFile:
+            customStyles = themeFile.read().replace('\n', '')
+            # Just a concatenation between stylesheets...
+            # it is slow but effective.
+
+            return customStyles
+            if _themeName != DEFAULT_STYLE_FNAME:
                 return _getBaseStylesheet() + customStyles
-        else:
-            print(type(_themeName))
-            print('Could not find theme: ' + _themeName + ' in the styles directory.')
-            return ""
+            else:
+                return customStyles
+    else:
+        print(type(_themeName))
+        print('Could not find theme: ' + _themeName + ' in the styles directory.')
+        return ""
 
 
-"""
-Prepares the given widget to automatically update when the stylesheet changes.
-Additionally, this sets the widget's current style sheet according to 
-the `ThemeName` set to `Configuration`.
-This assumes that a .css file exists in the appropriate directory. 
-"""
+
 def subscribeToStylesheet(widget):
+    """
+    Prepares the given widget to automatically update when the stylesheet changes.
+    Additionally, this sets the widget's current style sheet according to
+    the `ThemeName` set to `Configuration`.
+    This assumes that a .css file exists in the appropriate directory.
+    """
+
     global subscribers
     ref = weakref.ref(widget)
     subscribers.append(ref)
-    
-    widget.setStyleSheet(_getStyleSheet(Configuration.getSetting("ThemeName")))
+    theme_content = _getStyleSheet(Configuration.getSetting("ThemeName"))
+    if theme_content.strip():
+        widget.setStyleSheet(theme_content)
 
 
-"""
-This method removes all garbage-collected references from `subscribers`.
-It is not necessary. It just prevents `subscribers` from being full of `None`.
-"""
-def _cleanupSubscribers():
+
+def _cleanup_subscribers():
+    """
+    This method removes all garbage-collected references from `subscribers`.
+    It is not necessary. It just prevents `subscribers` from being full of `None`.
+    """
     global subscribers
-    if len(subscribers) > 100: #arbitrary; makes this method fire only periodically
-        toKeep = []
+
+    # arbitrary; makes this method fire only periodically
+    if len(subscribers) > 100:
+        to_keep = []
         for ref in subscribers:
             if ref():
-                toKeep.append(ref)
-        subscribers = toKeep
+                to_keep.append(ref)
+        subscribers = to_keep
 
 
-"""
-Sets the current theme of all widgets subscribed to stylesheet changes.
-"""
+
 def publishStylesheet(themeName):
-    _cleanupSubscribers()
+    """
+    Sets the current theme of all widgets subscribed to stylesheet changes.
+    """
+    return
+    _cleanup_subscribers()
     stylesheet = _getStyleSheet(themeName)
     for widget in subscribers:
         if widget():
