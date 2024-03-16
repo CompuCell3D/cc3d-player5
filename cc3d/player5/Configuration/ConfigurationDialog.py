5d0601b368be29258900c7b21369a7403ee0ebb7
Pete Fyffe <pgf01@rocketmail.com>
2024-03-15 21:18:27 -0400

    Hotfix for movie crashes; replaced os with pathlib in movie code

--- cc3d/player5/Configuration/ConfigurationDialog.py
+++ cc3d/player5/Configuration/ConfigurationDialog.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from pathlib import Path
 from cc3d import CompuCellSetup
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -146,7 +147,7 @@ class ConfigurationDialog(QDialog, ui_configurationdlg.Ui_CC3DPrefs, Configurati
 
     def chooseMovieDirectory(self):
         currentProjectDir = Configuration.getSetting('OutputLocation')
-        if not currentProjectDir or not os.path.exists(currentProjectDir):
+        if not currentProjectDir or not Path(currentProjectDir).exists():
             currentProjectDir = "/"
         dirName = QFileDialog.getExistingDirectory(self, "Specify CC3D Project Directory", currentProjectDir,
                                                     QFileDialog.ShowDirsOnly)
@@ -155,19 +156,17 @@ class ConfigurationDialog(QDialog, ui_configurationdlg.Ui_CC3DPrefs, Configurati
         if dirName == "":
             return None
 
-        simulationPath = os.path.abspath(dirName)
+        simulationPath = Path(dirName).resolve()
 
         hasProjectFile = False
         hasSimulationDir = False
 
-        for fileName in os.listdir(simulationPath):
-            filePath = os.path.join(simulationPath, fileName)
-            if os.path.isdir(filePath):
-                if fileName == "Simulation":
+        for p in simulationPath.glob('*'):
+            if p.is_dir():
+                if p.stem.lower() == "simulation":
                     hasSimulationDir = True
             else:
-                _, fileExtension = os.path.splitext(filePath)
-                if fileExtension.lower() == ".cc3d":
+                if p.suffix.lower() == ".cc3d":
                     hasProjectFile = True
 
         if not hasSimulationDir or not hasProjectFile:
@@ -183,29 +182,36 @@ class ConfigurationDialog(QDialog, ui_configurationdlg.Ui_CC3DPrefs, Configurati
 
 
     def createMovieButtonClicked(self):
-        self.createMovieResultLabel.setText("")
-        if not os.path.exists(Configuration.getSetting('FfmpegLocation')):
-            self.showFfmpegWarning()
-            return
+        try:
+            self.createMovieResultLabel.setText("")
+            if not Path(Configuration.getSetting('FfmpegLocation')).exists():
+                self.showFfmpegWarning()
+                return
 
-        simulationPath = self.chooseMovieDirectory()
-        if not simulationPath:
-            return
+            simulationPath = self.chooseMovieDirectory()
+            if not simulationPath:
+                return
 
-        self.createMovieResultLabel.setText("Generating movies...")
+            self.createMovieResultLabel.setText("Generating movies...")
 
-        frameRate = max(self.frameRateSpinBox.value(), 1)
-        quality = float(self.movieQualitySpinBox.value())
-        quality = max(quality, 1)
-        # Convert from 1-10 domain to 0-51 domain
-        quality = int((1.0 - (quality / 10.0)) * 52.0) - 1
-        enableDrawingMCS = self.writeMCSCheckbox.isChecked()
+            frameRate = max(self.frameRateSpinBox.value(), 1)
+            quality = float(self.movieQualitySpinBox.value())
+            quality = max(quality, 1)
+            # Convert from 1-10 domain to 0-51 domain
+            quality = int((1.0 - (quality / 10.0)) * 52.0) - 1
+            enableDrawingMCS = self.writeMCSCheckbox.isChecked()
 
-        movieCount, moviePath = makeMovie(simulationPath, frameRate, quality, enableDrawingMCS)
+            movieCount, moviePath = makeMovie(simulationPath, frameRate, quality, enableDrawingMCS)
 
-        Configuration.setSetting("RecentMoviePath", moviePath)
-
-        self.displayMovieResult(movieCount)
+            Configuration.setSetting("RecentMoviePath", str(moviePath.resolve()))
+            
+            self.displayMovieResult(movieCount)
+        except Exception as e:
+            print(e)
+            QMessageBox.warning(None, "WARN",
+                            "There was a problem creating the movie. "
+                            "Please reach out to us on Reddit or GitHub Issues if this problem persists.",
+                            QMessageBox.Ok)
 
 
     def displayMovieResult(self, movieCount):
--- cc3d/player5/Plugins/ViewManagerPlugins/MovieMediator.py
+++ cc3d/player5/Plugins/ViewManagerPlugins/MovieMediator.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import sys
 import subprocess
 import cc3d.player5.Configuration as Configuration
@@ -6,47 +6,56 @@ from PyQt5.QtWidgets import QMessageBox
 from cc3d.core.GraphicsUtils.MovieCreator import makeMovie
 
 def makeMovieWithSettings():
-    # Choose the most recently modified subdir of the project dir
-    projectPathRoot = Configuration.getSetting("OutputLocation")
-    maxLastModifiedTime = 0
-    simulationPath = None
-    for dirName in os.listdir(projectPathRoot):
-        dirPath = os.path.join(projectPathRoot, dirName)
-        if os.path.isdir(dirPath):
-            if os.path.getmtime(dirPath) > maxLastModifiedTime:
-                maxLastModifiedTime = os.path.getmtime(dirPath)
-                simulationPath = dirPath
+    try:
+        # Choose the most recently modified subdir of the project dir
+        projectPathRoot = Configuration.getSetting("OutputLocation")
+        projectPathRoot = Path(projectPathRoot)
+        maxLastModifiedTime = 0
+        simulationPath = None
+        for dirName in projectPathRoot.glob('*'):
+            dirPath = projectPathRoot.joinpath(dirName)
+            if dirPath.is_dir():
+                if dirPath.lstat().st_mtime > maxLastModifiedTime:
+                    maxLastModifiedTime = dirPath.lstat().st_mtime
+                    simulationPath = dirPath
 
-    frameRate = Configuration.getSetting("FrameRate")
-    quality = Configuration.getSetting("Quality")
-    writeText = Configuration.getSetting("WriteMovieMCS")
+        frameRate = Configuration.getSetting("FrameRate")
+        quality = Configuration.getSetting("Quality")
+        writeText = Configuration.getSetting("WriteMovieMCS")
 
-    numMoviesMade, moviePath = makeMovie(simulationPath, frameRate, quality, writeText)
+        numMoviesMade, moviePath = makeMovie(simulationPath, frameRate, quality, writeText)
 
-    if numMoviesMade > 0 and moviePath:
-        Configuration.setSetting("RecentMoviePath", moviePath)
+        if numMoviesMade > 0 and moviePath:
+            Configuration.setSetting("RecentMoviePath", str(moviePath.resolve()))
 
-    return numMoviesMade, moviePath
+        return numMoviesMade, moviePath
+    except:
+        print("There was a problem generating the movie. You can try generating another one from the Configuration menu.")
 
 
 def showMovieInFileExplorer(self):
-    if Configuration.check_if_setting_exists("RecentMoviePath"):
-        dirToOpen = Configuration.getSetting("RecentMoviePath")
-    elif Configuration.check_if_setting_exists("OutputLocation"):
-        dirToOpen = Configuration.getSetting("OutputLocation")
+    try:
+        if Configuration.check_if_setting_exists("RecentMoviePath"):
+            dirToOpen = Configuration.getSetting("RecentMoviePath")
+        elif Configuration.check_if_setting_exists("OutputLocation"):
+            dirToOpen = Configuration.getSetting("OutputLocation")
 
-    if not os.path.exists(dirToOpen):
-        dirToOpen = "/"
+        if not Path(dirToOpen).exists():
+            dirToOpen = "/"
 
-    if sys.platform.startswith('win'):
-        subprocess.run(['explorer', '/select,', dirToOpen], shell=True)
-    elif sys.platform.startswith('darwin'):
-        subprocess.run(['open', '-R', dirToOpen])
-    elif sys.platform.startswith('linux'):
+        if sys.platform.startswith('win'):
+            subprocess.run(['explorer', '/select,', dirToOpen], shell=True)
+        elif sys.platform.startswith('darwin'):
+            subprocess.run(['open', '-R', dirToOpen])
+        elif sys.platform.startswith('linux'):
+            QMessageBox.warning(None, "WARN",
+                                "Linux is not supported.",
+                                QMessageBox.Ok)
+        else:
+            QMessageBox.warning(None, "WARN",
+                                "Your operating system is not supported.",
+                                QMessageBox.Ok)
+    except:
         QMessageBox.warning(None, "WARN",
-                            "Linux is not supported.",
-                            QMessageBox.Ok)
-    else:
-        QMessageBox.warning(None, "WARN",
-                            "Your operating system is not supported.",
+                            "Sorry, opening your file explorer failed.",
                             QMessageBox.Ok)
